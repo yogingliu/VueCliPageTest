eb10369c0b968b080b07a727c8db839716907f94
BigWhite <bigwhite0321@gmail.com>
2022-09-27 14:55:59 +0800

    update: public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'development'? '/': '/VueCliPageTest/dist',
+  publicPath: '/VueCliPageTest/dist',
 })
